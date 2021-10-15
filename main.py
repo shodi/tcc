97a6cc7688859ef634127c3d184cd36b65fdab11
Shodi <hikihenrique@yahoo.com>
2021-10-15 18:25:53 -0300

    Calculando similaridade para o rkrgst

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from os import replace
 import subprocess
 import re
 from rkr_gst.RKRGST import RKRGST
@@ -6,10 +5,9 @@ from rkr_gst.RKRGST import RKRGST
 def source_code_to_token(source_code_path, output_filename = None):
     result = subprocess.run(['java', '-cp', '../../tcc/antlr:../../tcc/antlr/antlr-4.9.2-complete.jar', 'org.antlr.v4.gui.TestRig', 'C', 'translationUnit', '-tokens', source_code_path], stdout=subprocess.PIPE)
     raw_tokenized = result.stdout.decode('utf-8')
-    print(raw_tokenized)
     code = ''
     for token in raw_tokenized.split('\n'):
-        match, *_ = re.findall(r'(?<=\<).+?(?=\>)', token)
+        match, *_ = re.findall(r'(?<=\,\<).+?(?=\>\,)', token)
         if match[0] == '\'':
             match = match[1:-1]
         if match == 'EOF':
@@ -19,6 +17,7 @@ def source_code_to_token(source_code_path, output_filename = None):
             code += match + '\n'
             continue
         code += match + ' ' if not is_char else match + '\n'
+    code = re.sub(r'[\{\}\;\n]', '', code)
     if output_filename:
         with open(output_filename, 'w') as token_file:
             token_file.write(code)
@@ -27,4 +26,9 @@ def source_code_to_token(source_code_path, output_filename = None):
 if __name__ == '__main__':
     code1 = source_code_to_token('../../tcc/antlr/example.c', './out1')
     code2 = source_code_to_token('../../tcc/antlr/example2.c', './out2')
-    print(RKRGST.run(code1, code2, 3, 10))
+    matches = RKRGST.run(code1, code2, 3, 9)
+    sim = 0
+    code_len = len(code1)
+    for match in matches:
+        sim += 100 * match.length / code_len
+    print('Similaridade: {}'.format(sim))
